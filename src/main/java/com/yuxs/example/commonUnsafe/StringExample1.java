5c7eb80d6f6619ce92414567a332ba4372d341fe
daxiaojiuyouqu <yuxsh@deayea.com>
2018-08-15 17:27:42 +0800

    first commit

--- src/main/java/com/yuxs/example/commonUnsafe/StringExample1.java
+++ src/main/java/com/yuxs/example/commonUnsafe/StringExample1.java
@@ -1,4 +1,44 @@
 package com.yuxs.example.commonUnsafe;
 
+import com.yuxs.ConcurrencyTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
 public class StringExample1 {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConcurrencyTest.class);
+
+    public static final int clienTotal = 5000;
+
+    public static final int threadTotal = 200;
+
+    public static StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clienTotal);
+        for (int i = 0; i < clienTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        logger.info("len={}", stringBuilder.length());
+    }
+
+    private static void update() {
+        stringBuilder.append("1");
+    }
 }
